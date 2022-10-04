f423ffd11026a8dfadf4134872b87e19975bb15a
albertoalmenara <PeopleRK>
2022-10-04 09:20:23 +0200

    Correcciones Nivel 3 / ejercicio 7

--- nivel3/ejercicio7/js/main.js
+++ nivel3/ejercicio7/js/main.js
@@ -2,6 +2,7 @@
 var burger = document.getElementById('burger');
 var menu = document.getElementById('menu');
 var logo = document.getElementById('logo');
+var form = document.getElementById('contact-form');
 
 burger.addEventListener('click', openMenu);
 
@@ -10,3 +11,44 @@ function openMenu(){
     menu.classList.toggle("opened");
     logo.classList.toggle("opened");
 }
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+// function ValidateEmail(input) {
+    
+//     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+//     if (input.match(validRegex)) {
+//         document.form1.text1.focus();
+//         return true;
+//     } 
+//     else {
+//         document.form1.text1.focus();
+//         return false;
+//     }
+// }
+// form.addEventListener("submit", function(e){
+//     email = document.getElementById('email').value;
+    
+//     form.preventDefault();
+//     alert(email)
+//     if(!ValidateEmail(email)){
+//         form.classList.add('was-validated');
+//     }
+// });
+
